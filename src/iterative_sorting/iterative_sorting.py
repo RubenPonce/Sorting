1981ceabc1c6c839de82c744f92778c48d6b4972
Ruben Ponce <rubenmartinponce@gmail.com>
2020-02-27 12:58:46 -0500

    finish bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -28,31 +28,27 @@ def selection_sort( arr ):
                 smallest_index = remaining_index
         if(smallest_index != currentIndex):
             arr[currentIndex], arr[smallest_index] =  arr[smallest_index], arr[currentIndex]
-            
-        
-        #because smallest index and current index are always the same, the first element is always a minimum. 
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-
-             
-
-
-
-        # TO-DO: swap
-        
-            
-
-
 
     print(arr)
     return arr
 
-selection_sort(example_arr)
+# selection_sort(example_arr)
 # TO-DO:  implement the Bubble Sort function below
+def swap(arr, currIndex, remainIndex):
+    temp = arr[currIndex]
+    arr[currIndex], arr[remainIndex] = arr[remainIndex], temp
 def bubble_sort( arr ):
-
+    #if the current element greater than the nextelement, swap it
+    for current_index in range(len(arr)):
+        for remaining_index in range(len(arr)-current_index - 1):
+            if(arr[remaining_index] > arr[remaining_index + 1]):
+                temp = arr[remaining_index]
+                arr[remaining_index], arr[remaining_index + 1] = arr[remaining_index+1], temp
+            
+      
+    print("bubble_sort", example_arr)
     return arr
-
+# bubble_sort(example_arr)
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
